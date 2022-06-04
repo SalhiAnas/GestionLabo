bd69b0e82c11b674409b8e19391de471432943ab
anas <salhi6@gmail.com>
2022-06-04 13:38:41 +0100

    First Commit!

---
+++ src/main/java/com/gestionlabo/gestionlabo/GestionLaboApplication.java
@@ -0,0 +1,13 @@
+package com.gestionlabo.gestionlabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLaboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionLaboApplication.class, args);
+    }
+
+}
